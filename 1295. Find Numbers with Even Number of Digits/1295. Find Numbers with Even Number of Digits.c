86ee4374b065123b0fb724a033b88a579b79e832
Kartik Bhartiya <kartikbhartiya613@gmail.com>
2025-06-10 00:45:01 +0530

    Create 1295. Find Numbers with Even Number of Digits.c

---
+++ 1295. Find Numbers with Even Number of Digits/1295. Find Numbers with Even Number of Digits.c
@@ -0,0 +1,21 @@
+int findNumbers(int* nums, int numsSize) {
+    int ans = 0;
+    for (int i = 0; i < numsSize; i++){
+       int d = (int)log10(nums[i]) + 1;
+       if (d % 2 == 0) ans++;
+    }
+    return ans;
+}
+
+
+//I have applied another logic based on fixed constrained mentioned below
+
+int findNumbers(int* arr, int numsSize) {
+    int output = 0;
+    for (int i = 0; i < numsSize; i++) {
+        int n = arr[i];
+        if ((n >= 10 && n <= 99) || (n >= 1000 && n <= 9999) || n == 100000)
+            output++;
+    }
+    return output;
+}
